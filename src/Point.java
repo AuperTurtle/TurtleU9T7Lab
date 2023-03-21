d28a05f3532e2369304c4d19fce3acbe43b63e73
1E10 Laptop <sample@bths.edu>
2023-03-21 10:25:07 -0400

    .

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "Point " + label + " is at x = " + x + ", y = " + y;
+    }
+
+    @Override
+    public boolean equals(Object temp) {
+        if (temp instanceof Point) {
+            Point tempPoint = (Point) temp;
+            if (tempPoint.x != x) {
+                return false;
+            }
+            if (tempPoint.y != y) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- src/ThreeDPoint.java
+++ src/ThreeDPoint.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ThreeDPoint {
+public class ThreeDPoint extends Point {
+
+    private int z;
+
+    public ThreeDPoint(String label, int x, int y, int z) {
+        super(label, x , y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", z = " + z;
+    }
+
+    @Override
+    public boolean equals(Object temp) {
+        if (temp instanceof ThreeDPoint) {
+            ThreeDPoint tempPoint = (ThreeDPoint) temp;
+            if (super.equals(temp) && tempPoint.z == z) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
